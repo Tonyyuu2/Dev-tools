86ccb412a5eec260bad2ce2e7ccb4b864e807fa7
lucky <lucky.hw.kim@gmail.com>
2022-05-12 22:10:29 -0700

    new branch / add db table

---
+++ server/src/db/db.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const { Pool } = require("pg").Pool;
+const config = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT
+  }
+const pool = new Pool(config);
