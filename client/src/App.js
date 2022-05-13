1e06ce91e8cb67e5abb7241de8c83c9fbc7513ca
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-05-13 16:53:27 -0700

    add journal and pomodoro timer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import "./App.css";
+import Form from './components/add_journal/Form'
+import Timer from "./components/pomodoro/Timer";
+import Settings from "./components/pomodoro/Settings";
+import Settingscontext from "./components/pomodoro/Settingscontext";
+import { useState } from "react"
 
 function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
   return (
-    <div className="App">
+    <>
+    <main>
+      <Settingscontext.Provider value={{
+        showSettings,
+        workMinutes,
+        breakMinutes,
+        setShowSettings,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </Settingscontext.Provider>
+    </main>
+    <div>
+      <Form/>
     </div>
+    </>
   );
 }
-
 export default App;
