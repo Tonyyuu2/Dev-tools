f8af84b60772c5dca8de4a2260e98cc8bb890a22
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-05-15 14:30:38 -0700

    pushing same stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,12 @@ import './App.css';
 import Backcare from './components/backcare/Backcare';
 import Journal from './components/journal/Journal';
 import Form from './components/journal/Form';
-<<<<<<< HEAD
-import Pomodoro from './components/pomodoro/Pomodoro';
-import TodoList from './components/to-do-list/TodoList';
-
-
-
-=======
 import Pomodoro from './components/pomodoro/Pomodoro'
 import WeatherComponent from './components/weather/WeatherComponent'
->>>>>>> 101c08b56306f72d66df9d5a9ce6929c0fdcfc53
 
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-    </>
-=======
     <div className="App">
       {/* <Backcare /> */}
         {/* <Journal />  */}
@@ -27,7 +15,6 @@ function App() {
       {/* <Form /> */}
       <WeatherComponent/>
     </div>
->>>>>>> 101c08b56306f72d66df9d5a9ce6929c0fdcfc53
   );
 };
 
