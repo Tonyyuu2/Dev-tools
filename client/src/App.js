4f301890fdbbac7524aa546fa1ff40b672054725
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-05-15 13:05:02 -0700

    add weather feature

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import Backcare from './components/backcare/Backcare';
 import Journal from './components/journal/Journal';
 import Form from './components/journal/Form';
 import Pomodoro from './components/pomodoro/Pomodoro'
+import WeatherComponent from './components/weather/WeatherComponent'
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Backcare /> */ }
-      {/*  <Journal /> */ }
-      <Pomodoro />
+      {/* <Backcare /> */}
+        {/* <Journal />  */}
+      {/* <Pomodoro /> */}
+      {/* <Form /> */}
+      <WeatherComponent/>
     </div>
   );
 };
