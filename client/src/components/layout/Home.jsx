f73cb569bddacc24be989f1ec6bf0948f19e2e47
lucky <lucky.hw.kim@gmail.com>
2022-05-15 16:15:20 -0700

    change style

--- client/src/components/layout/Home.jsx
+++ client/src/components/layout/Home.jsx
@@ -1,11 +1,15 @@
 import Login from "../spotify/Login";
 import "./Home.scss";
+import styles from "./Home.module.css"
 
 const Home = () => {
   return (
     <>
-      <div className="homeLogin">
-        {/* <Login></Login> */}
+      <div className={styles.homePage}>
+        <div className={styles.widget1}></div>
+        <div className={styles.widget2}></div>
+        <div className={styles.widget3}></div>
+        <div className={styles.widget4}></div>
       </div>
     </>
   );
