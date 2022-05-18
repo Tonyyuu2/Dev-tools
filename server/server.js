300b5214626ea1bea54cad618f783c1af287afb9
lucky <lucky.hw.kim@gmail.com>
2022-05-18 11:41:41 -0700

    merge

--- server/server.js
+++ server/server.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-const db = require("./src/db/db");
-const journalEntruyRoute = require("./src/routes/journalEntryRoute");
-const todoListRoute = require("./src/routes/todoListRoute");
-require('dotenv').config();
-
-
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const pool = require("./db");
-const PORT = process.env.PORT;
-
-
-//middleware
-app.use(cors());
-app.use(express.json());
-
-// Sample GET route
-app.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
-});
-
-=======
 require("dotenv").config();
 
 
@@ -49,4 +20,3 @@ app.use('/api/datalab', require('./routes/datalabRoutes'));
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
->>>>>>> feature/merge
