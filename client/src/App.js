d5c61f20a6ccf6718b4dfdd202c455a8cc57c024
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-05-19 20:25:10 -0700

    created about us page

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout';
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import Home from "./components/layout/Home";
 import Timer from "./components/layout/Timer";
 import Wecare from "./components/layout/Wecare";
-import Break from './components/layout/Break';
-import About from './components/layout/About';
-
+import Break from "./components/layout/Break";
+import About from "./components/layout/About";
 
 function App() {
   return (
     <>
       <Routes>
-
-        <Route path="/" element={ <Layout /> } >
-          <Route index element={ <Home /> } />
-          <Route path='about' element={ <About /> } />
-          <Route path='timer' element={ <Timer /> } />
-          <Route path='backcare' element={ <Wecare /> } />
-          <Route path='break' element={ <Break /> } />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="timer" element={<Timer />} />
+          <Route path="backcare" element={<Wecare />} />
+          <Route path="break" element={<Break />} />
         </Route>
       </Routes>
     </>
   );
-};
+}
 
 export default App;
--- client/src/components/layout/About.js
+++ client/src/components/layout/About.js
@@ -1,35 +1,104 @@
 import React from "react";
 import styles from "../../App.module.css";
 import classes from "./About.module.css";
-import Tony from "./assets/Tony.jpg";
-import { FaGithub } from "react-icons/fa";
+import Tony from "./assets/Tony.png";
+import Lucky from "./assets/Lucky.png";
+import Shubham from "./assets/Shubham.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 export default function About() {
   return (
     <>
       <div className={styles.global_bg}>
         <header className={classes.header}>
-          <h1>For Devs</h1>
-          <h1>By Devs</h1>
+          <div className={classes.missionstatement}>
+          <p>DevTools is an app designed to help developers be productive and stay productive. We wanted to provide a space where devs have access to a suite of tools ready-to-go without the nuisance of tabbing from window to window. Our tools are tailor-made to make a dev’s life easier so they can focus on what’s important: writing code, and building great software.</p>
+          </div>
           <div className={classes.borderforheader}></div>
         </header>
-        <div className={classes.container}>
-          <div>
-            <img src={Tony} className={classes.img}></img>
+        <div className={classes.cards}>
+          <div className={classes.container}>
+            <div>
+              <img src={Tony} className={classes.img}></img>
+            </div>
+            <div className={classes.name}>
+              <h2>Tony</h2>
+            </div>
+            <div className={classes.description}>Full Stack Web Developer</div>
+            <IconContext.Provider value={{ color: "grey", size: "40px" }}>
+              <div className={classes.atag}>
+                <a
+                  href="https://github.com/Tonyyuu2"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub className={classes.icon} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/tony-yu-23676a212/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaLinkedin className={classes.icon} />
+                </a>
+              </div>
+            </IconContext.Provider>
           </div>
-          <div className={classes.name}>
-            <h2>Tony</h2>
+          <div className={classes.container}>
+            <div>
+              <img src={Shubham} className={classes.img}></img>
+            </div>
+            <div className={classes.name}>
+              <h2>Shubham</h2>
+            </div>
+            <div className={classes.description}>Full Stack Web Developer</div>
+            <IconContext.Provider value={{ color: "grey", size: "40px" }}>
+              <div className={classes.atag}>
+                <a
+                  href="https://github.com/shubham2295"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub className={classes.icon} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/shubham-patel-22sm95/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaLinkedin className={classes.icon} />
+                </a>
+              </div>
+            </IconContext.Provider>
           </div>
-          <div className={classes.description}>Full Stack Web Developer</div>
-          <div className={classes.atag}>
-            <a
-              href="https://github.com/Tonyyuu2"
-              target="_blank"
-              rel="noreferrer"
-              className={classes.atag}
-            >
-              <FaGithub className={classes.icon} size={50}/>
-            </a>
+          <div className={classes.container}>
+            <div>
+              <img src={Lucky} className={classes.img}></img>
+            </div>
+            <div className={classes.name}>
+              <h2>Lucky</h2>
+            </div>
+            <div className={classes.description}>Full Stack Web Developer</div>
+            <IconContext.Provider value={{ color: "grey", size: "40px" }}>
+              <div className={classes.atag}>
+                <a
+                  href="https://github.com/lucky-hw-kim"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub className={classes.icon} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/luckykim/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaLinkedin className={classes.icon} />
+                </a>
+              </div>
+            </IconContext.Provider>
+          <div className={classes.spacer}></div>
           </div>
         </div>
       </div>
