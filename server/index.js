8d8cff3bdbf2753453ef3fce654d38dd23166780
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-05-20 14:44:47 -0700

    fixing merge issues

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT || 8081;
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
