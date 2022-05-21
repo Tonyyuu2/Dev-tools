1eb1a1c33be0e6e76943c67c6fc7f2ad85b9bba9
shubhampatel <shubhampatel2295@gmail.com>
2022-05-21 16:53:19 -0600

    Register page styling

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, NavLink } from 'react-router-dom';
 import AuthContext from '../store/auth-context';
 import classes from './Login.module.css';
 import style from '../../App.module.css';
@@ -30,23 +30,23 @@ const Login = () => {
 
     <div className={ style.global_bg }>
       <div className={ classes.main_body }>
-      <h1 className={ classes.welcome_container }>Welcome <br/> Back!</h1>
-      <div className={ classes.container } >
-        <form onSubmit={ handleSubmit } className={ classes.form_container }>
-        <h3 className={ classes.login }>Login</h3>
-          <label htmlFor='email'></label>
-          <input className={ classes.inputField } type='email' name='email' onChange={ handleChange } placeholder="email address"></input>
-          <label htmlFor='password'></label>
-          <input className={ classes.inputField } type='password' placeholder='password' name='password' onChange={ handleChange }></input>
-          { error && <p>{ error }</p> }
-          <button type='submit' className={ classes.btn }>Login</button>
-          <div className={ classes.newUser}>
-          <p >New User? </p>
-          <p>Forgot Your Password?</p>
-          </div>
-        </form>
-    </div>
-    </div>
+        <h1 className={ classes.welcome_container }>Welcome <br /> Back!</h1>
+        <div className={ classes.container } >
+          <form onSubmit={ handleSubmit } className={ classes.form_container }>
+            <h3 className={ classes.login }>Login</h3>
+            <label htmlFor='email'></label>
+            <input className={ classes.inputField } type='email' name='email' onChange={ handleChange } placeholder="email address"></input>
+            <label htmlFor='password'></label>
+            <input className={ classes.inputField } type='password' placeholder='password' name='password' onChange={ handleChange }></input>
+            { error && <p>{ error }</p> }
+            <button type='submit' className={ classes.btn }>Login</button>
+            <div className={ classes.newUser }>
+              <p><NavLink to='/register'>New user?</NavLink></p>
+              <p>Forgot Your Password?</p>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/readmeHelper/WriteHelperTool.jsx
+++ client/src/components/readmeHelper/WriteHelperTool.jsx
@@ -1,31 +1,32 @@
-import styles from "./WriteHelperTool.module.css"
-import { useState } from "react";
-import Loader from "./Loader"
-const { Configuration, OpenAIApi } = require("openai");
+import styles from './WriteHelperTool.module.css';
+import { useState } from 'react';
+import Loader from './Loader';
+const { Configuration, OpenAIApi } = require('openai');
 
 export default function WriteHelperTool() {
+  const initStatement = (
+    <div className={styles.initStatement}>Description will be printed HERE</div>
+  );
 
-  const initStatement = <div className={styles.initStatement}>Description will be printed HERE</div>
-
-  const [response, setResponse] = useState(initStatement)
-  const [isLoading, setIsLoading] = useState(false)
+  const [response, setResponse] = useState(initStatement);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onFormSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const formData = new FormData(e.target),
-    formDataObj = Object.fromEntries(formData.entries())
+      formDataObj = Object.fromEntries(formData.entries());
 
-    console.log(process.env.REACT_APP_API_KEY)
+    console.log(process.env.REACT_APP_API_KEY);
     const configuration = new Configuration({
       apiKey: process.env.REACT_APP_API_KEY,
     });
 
-  const openai = new OpenAIApi(configuration);
+    const openai = new OpenAIApi(configuration);
 
-
-      setIsLoading(true);
-      openai.createCompletion("text-davinci-002", {
+    setIsLoading(true);
+    openai
+      .createCompletion('text-davinci-002', {
         prompt: `Write me a detailed and professional description of my project that has a name of ${formDataObj.projectTitle}, uses ${formDataObj.techStack} as tech stacks and has functionalities of ${formDataObj.functionalities}.\n`,
         temperature: 0.7,
         max_tokens: 230,
@@ -35,57 +36,63 @@ export default function WriteHelperTool() {
       })
       .then((result) => {
         setIsLoading(false);
-        setResponse(`${result.data.choices[0].text}`)
-      })
-  }
-
-
+        setResponse(`${result.data.choices[0].text}`);
+      });
+  };
 
   return (
     <>
-    <div className={styles.main_container}>
-      <h1 className={styles.heading}>Project Description Generator</h1>
-      <p className={styles.subheading}>Generate your project description for <b>README</b></p>
+      <div className={styles.main_container}>
+        <h1 className={styles.heading}>Project Description Generator</h1>
+        <p className={styles.subheading}>
+          Generate your project description for <b>README</b>
+        </p>
+        <form onSubmit={onFormSubmit}>
+          <div className={styles.formGroup}>
+            <input
+              type='text'
+              placeholder='Project Title'
+              name='projectTitle'
+              className={styles.formInput}
+            />
+            <input
+              type='text'
+              placeholder='Tech Stack'
+              name='techStack'
+              className={styles.formInput}
+            />
+            <input
+              type='text'
+              placeholder='Functionalities'
+              name='functionalities'
+              className={styles.formInput}
+            />
+          </div>
+          <div className={styles.btnGroup}>
+            <button
+              className={styles.btnGenerate}
+              variant='primary'
+              type='submit'
+              disabled={isLoading}
+            >
+              Generate
+            </button>
+            <button className={styles.btnReset} type='reset'>
+              Clear
+            </button>
+          </div>
+        </form>
 
-      <form onSubmit={onFormSubmit}> 
-        <div className={styles.formGroup} controlId="formBasicEmail">
-          <input
-          type="text" 
-          placeholder="Project Title"
-          name="projectTitle" className={styles.formInput}  />
-          <input
-          type="text" 
-          placeholder="Tech Stack"
-          name="techStack" className={styles.formInput} />
-          <input
-          type="text" 
-          placeholder="Functionalities"
-          name="functionalities" className={styles.formInput} />
-        </div>
-        <div className={styles.btnGroup}>
-          <button className={styles.btnGenerate} variant="primary" type="submit" disabled={isLoading} >
-            Generate
-          </button>
-          <button className={styles.btnReset} type="reset">
-            Clear
-          </button>
-        </div>
-      </form> 
-
-      <br />
-      <br />
-      <div className={styles.divContainer}>
-        <div>
-          <div className={styles.description}>
+        <br />
+        <br />
+        <div className={styles.divContainer}>
+          <div>
+            <div className={styles.description}>
               {isLoading ? <Loader /> : response}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
-
-
-
-
